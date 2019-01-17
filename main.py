12e4a9deaf74f067dc0120b637c18e704d3b6d42
vicda164 <victor.dannetun@live.com>
2019-01-17 02:43:38 -0800

    validates against non-trained NER, latex format on results

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import graph
 import silver_standard as silver
 import train
 
-def find_family_relations(to_read=[], read_bios=[], relations=[]):
+def find_family_relations(to_read=[], read_bios=[], relations=[], model="./model"):
     """
         Recursivly read documents of characters that occur in a text from the root name.
         For each name extract relations.
@@ -25,7 +25,7 @@ def find_family_relations(to_read=[], read_bios=[], relations=[]):
     to_read.remove(name)
 
     if char_bio != None:
-        rel = relation_extraction.extract_relations(char_bio)
+        rel = relation_extraction.extract_relations(char_bio, model)
     
         if rel is None or rel == []:
             if len(to_read) > 0:
@@ -100,43 +100,78 @@ def _validate(family_set, silver_set):
     """
 
     print("_validate:")
-    print("family:" ,family_set)
-    print("silver:" ,silver_set)
+    #print("family:" ,family_set)
+    #print("silver:" ,silver_set)
 
     false_positive = []
     true_positive = []
-
+    false_relation_but_correct_pair = []
+    family = []
     for relation in family_set:
         #print(relation)
         rel = relation[2]["relation"]
         edge = (relation[0], relation[1], {"relation":rel})
+        family.append(edge)
         if edge in silver_set:
-            true_positive.append(edge)
+            true_positive.append(edge)            
         else:
-            false_positive.append(edge)
+            false_positive.append(edge)        
+
+    count = 0
+    # TODO: validate if correct. do tests
+    for relation in false_positive:
+        for s in silver_set:
+            count += 1
+            if edge[0:2] == s[0:2]:
+                false_relation_but_correct_pair.append(edge)
+                break
+    #print("fp:", len(false_relation_but_correct_pair))
+    print(count)
+
 
     if len(family_set) == 0:
-        return (0,0,0) 
+        return (0,0,0,0,0,0)
     recall = len(true_positive) / len(silver_set)
     precision = len(true_positive) / len(family_set)
     if recall + precision == 0:
-        return (0,0,0)
+        return (0,0,0,0,0,0)
     f1 = (2 * recall * precision) / (recall + precision)
 
+    silver_minus_intersect = [i for i in silver_set if i not in true_positive]
+    family_minus_intersect = [i for i in family if i not in true_positive]
+    recall_pair = len(false_relation_but_correct_pair) / len(silver_minus_intersect)
+    precision_pair = len(false_relation_but_correct_pair) / len(family_minus_intersect)
+    if recall_pair + precision_pair == 0:
+        return (recall, precision, f1,0,0,0)
+
+    f1_pair = (2 * recall_pair * precision_pair) / (recall_pair + precision_pair)
     print("Length of validation set:", len(silver_set))
     print("Length of family set:", len(family_set))
 
-    return (recall, precision, f1)
+    return (recall, precision, f1, recall_pair, precision_pair, f1_pair)
+
+def latex_tabell(name, family_no_model, family, silver):
+    # TODO add measure of # correct edges      
+    (recall0, precision0, f10, rp0, pp0, fp0) = _validate(family_no_model, silver)
+    (recall, precision, f1, rp, pp, fp) = _validate(family, silver)
+    print("\multicolumn{{1}}{{c}}{} & \\\ \cline{{1-3}}".format(name))
+    print("Measure     & Before training & After Training \\\ \hline")
+    print("Recall      & {:.2f}({:.2f})     & {:.2f}({:.2f})    \\\ ".format(recall0, rp0, recall, rp))
+    print("Precision   & {:.2f}({:.2f})     & {:.2f}({:.2f})     \\\ ".format(precision0, pp0, precision, pp))
+    print("F1-score    & {:.2f}({:.2f})     & {:.2f}({:.2f})     \\\ ".format(f10, fp0, f1, fp))
+    print("Relations   & {}({})     & {}({})      \\\ \hline".format(len(family_no_model), len(silver), len(family), len(silver)))
+    #print("".format())
 
 @plac.annotations(
     name=("Character name", "positional", None, str),
     #model=("Model to use", "option", "m")
     disbale_cache=("Delete family tree from root name Name", "flag", "r"),
     validate=("Validate", "flag", "v"),
+    svalidate=("Scientific validation, validate against untrained model and silver data", "flag", "V"),
     s=("Just get silver nothing else", "flag", "s"),
     draw=("Draw graph", "flag", "d"),
     fetchall=("Fetch all character desciptions of names in kaggle_data/Characters","flag","f"))
-def run(name, disbale_cache, validate, s, draw,fetchall):
+def run(name, disbale_cache, validate, svalidate, s, draw,fetchall):
     if fetchall:
         print("Fetch all descriptions")
         names = filemanager.lotr_char_names()
@@ -176,7 +211,6 @@ def run(name, disbale_cache, validate, s, draw,fetchall):
 
     print("RUN 1")    
     if validate:
-        #TODO BUG? get_silver_family is inconsistant
         silver = get_silver_family(name)
         (recall, precision, f1) = _validate(family, silver)
 
@@ -194,6 +228,26 @@ def run(name, disbale_cache, validate, s, draw,fetchall):
             file1 = "./data/family/" + name            
             graph.draw(file1) 
     
+
+    if svalidate:
+        family = None
+        family_no_model = None
+        silver = get_silver_family(name)
+
+        if name not in os.listdir("./data/family/"):
+            family_tree = find_family_relations(to_read=[name], model="./model")
+            family = graph.add_relations(family_tree, "./data/family/"+name)        
+        else:
+            family = graph.get("./data/family/"+name)
+
+        if name not in os.listdir("./data/family_no_model/"):
+            family_tree_no_model = find_family_relations(to_read=[name], model="en_core_web_sm")
+            family_no_model = graph.add_relations(family_tree_no_model, "./data/family_no_model/"+name)        
+        else:
+            family_no_model = graph.get("./data/family_no_model/"+name)
+
+        latex_tabell(name, family_no_model, family, silver)        
+
     print("MAIN DONE")
 
 
@@ -201,6 +255,7 @@ if __name__ == "__main__":
     if "data" not in os.listdir():
         os.makedirs("./data/character_bio")
         os.makedirs("./data/character_infobox")
+        os.makedirs("./data/family_no_model")
         os.makedirs("./data/family")
         os.makedirs("./data/silver")
     if "model" not in os.listdir():
